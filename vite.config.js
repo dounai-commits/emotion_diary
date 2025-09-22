f8c8c36715c0188aae88c1f78c5470fcb34cbd1b
dounai-commits <ashleycshi18@gmail.com>
2025-09-23 01:16:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // GitHub Pages 项目页必须使用仓库名作为 base
+  base: '/emotion_diary/',
 })
