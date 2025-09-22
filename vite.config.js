4c4994487f6a9eccaba4b21ac2011350abdee3fb
dounai-commits <ashleycshi18@gmail.com>
2025-09-23 00:05:43 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/emotion_diary/',   // 关键：项目页必须设 base
 })
